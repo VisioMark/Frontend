e1b55c6861a0a3cf257bf7edb097f5b87a1213b8
Domeh John Kelvin <75913666+JhohannesK@users.noreply.github.com>
2023-05-31 02:03:33 +0000

    create generic page for auth (#5)

---
+++ src/pages/auth-pages/styles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const MainContainer = styled.div`
+  display: flex;
+  padding: 2rem 40px;
+  justify-content: space-between;
+`;
+
+export const LeftContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  gap: 3rem;
+`;
+
+export const LogoContainer = styled.div``;
+
+export const Title = styled.h1`
+  font-family: Rubik;
+`;
+
+export const RightContainer = styled.div`
+  border: 1px solid red;
+  width: 50%;
+`;
