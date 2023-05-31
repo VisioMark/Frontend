eec6d417399d4284807b7f7f594d4dd65bd742e5
Domeh John Kelvin <75913666+JhohannesK@users.noreply.github.com>
2023-05-31 13:18:52 +0000

    add generic input to be used (#8)

--- src/pages/auth-pages/styles.ts
+++ src/pages/auth-pages/styles.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import styled from '@emotion/styled';
+import { TextInput } from '@mantine/core';
 
 export const MainContainer = styled.div`
   display: flex;
@@ -20,6 +21,24 @@ export const Title = styled.h1`
 `;
 
 export const RightContainer = styled.div`
-  border: 1px solid red;
+  padding: 7rem 0;
   width: 50%;
+`;
+
+export const FormTitle = styled.div``;
+
+export const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+`;
+
+export const GenicTextInput = styled(TextInput)`
+  & .mantine-inputWrapper {
+    background-color: 'white';
+  }
+
+  & .mantine-inputWrapper-label {
+    color: 'white';
+  }
 `;
