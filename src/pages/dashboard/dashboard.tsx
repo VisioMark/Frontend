0fc11bcbcf00646a2739a6e68f2df252ec266268
paakuubrako <108901730+paakuubrako@users.noreply.github.com>
2023-06-11 00:06:10 +0000

    Modal update (#20)

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -4,6 +4,9 @@ import Layout from '../common/components/Layout';
 import GenericBtn from '../common/components/button';
 import { RequestBtn } from './styles';
 import { useDisclosure } from '@mantine/hooks';
+import GenericInput from '../common/components/input';
+import { LogoWrapper } from '../common/components/layoutStyles';
+import { Title } from '../common/components/layoutStyles';
 
 const Dashboard = () => {
   const [opened, { open, close }] = useDisclosure(false);
@@ -12,7 +15,34 @@ const Dashboard = () => {
     <Layout>
       <RequestBtn>
         <ModalComp opened={opened} close={close}>
-          {<>hello</>}
+          {
+            <LogoWrapper>
+              <img src="/src/assets/logo.svg" width={40} alt="logo" />
+              <Title>visioMark</Title>
+            </LogoWrapper>
+          }
+
+          {<GenericInput placeholder="" label="Lecturer name" icon />}
+
+          {<GenericInput placeholder="" label="Course code" icon />}
+
+          {<GenericInput placeholder="" label="Department code" icon />}
+
+          {<GenericInput placeholder="" label="Year" icon />}
+
+          {<GenericInput placeholder="" label="Number of questions" icon />}
+          <br />
+          {
+            <GenericBtn
+              title="Done"
+              sx={{
+                height: '2rem',
+                width: '5rem',
+                fontSize: '1rem',
+                background: `${THEME.colors.button.primary}`,
+              }}
+            />
+          }
         </ModalComp>
         <GenericBtn
           title="Make a Request"
