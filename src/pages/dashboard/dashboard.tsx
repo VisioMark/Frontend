0d17697a83edf406899c606c3f9b8bdd0b90c2f5
jhohannesK <jdomeh77@gmail.com>
2023-06-21 13:13:40 +0000

    validate dashboard

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,3 +1,4 @@
+import { z } from 'zod';
 import { THEME } from '../../appTheme';
 import ModalComp from '../common/Modal/Modal';
 import SharedCard from '../common/components/Card/card';
@@ -13,9 +14,27 @@ import {
   Title,
 } from './styles';
 import { useDisclosure } from '@mantine/hooks';
+import { UserFormProvider, useUserForm } from '../common/form-context';
+import { zodResolver } from '@mantine/form';
+
+const schema = z.object({
+  course_code: z.string().min(6).max(6),
+  department_code: z.string().min(3).max(3),
+  year: z.string().datetime(),
+  number_of_questions: z.number().max(200),
+});
 
 const Dashboard = () => {
   const [opened, { open, close }] = useDisclosure(false);
+  const form = useUserForm({
+    validate: zodResolver(schema),
+    initialValues: {
+      course_code: '',
+      department_code: '',
+      year: '',
+      number_of_questions: '',
+    },
+  });
 
   return (
     <Layout>
@@ -29,51 +48,56 @@ const Dashboard = () => {
           }
 
           {
-            <ModalInputs>
-              <GenericInput
-                placeholder=""
-                val_name="course_code"
-                label="Course code"
-                textInput
-                icon
-              />
+            <UserFormProvider form={form}>
+              <form onSubmit={form.onSubmit((value) => console.log(value))}>
+                <ModalInputs>
+                  <GenericInput
+                    {...form.getInputProps('course_code')}
+                    placeholder=""
+                    val_name="course_code"
+                    label="Course code"
+                    textInput
+                    icon
+                  />
 
-              <GenericInput
-                val_name="department_code"
-                placeholder=""
-                label="Department code"
-                textInput
-                icon
-              />
+                  <GenericInput
+                    val_name="department_code"
+                    placeholder=""
+                    label="Department code"
+                    textInput
+                    icon
+                  />
 
-              <GenericInput
-                val_name="year"
-                placeholder=""
-                label="Year"
-                textInput
-                icon
-              />
+                  <GenericInput
+                    val_name="year"
+                    placeholder=""
+                    label="Year"
+                    textInput
+                    icon
+                  />
 
-              <GenericInput
-                placeholder=""
-                val_name="number_of_questions"
-                label="Number of questions"
-                textInput
-                icon
-              />
-            </ModalInputs>
-          }
-          <br />
-          {
-            <GenericBtn
-              title="Done"
-              sx={{
-                height: '2rem',
-                width: '5rem',
-                fontSize: '1rem',
-                background: `${THEME.colors.button.primary}`,
-              }}
-            />
+                  <GenericInput
+                    placeholder=""
+                    val_name="number_of_questions"
+                    label="Number of questions"
+                    textInput
+                    icon
+                  />
+                </ModalInputs>
+
+                <br />
+
+                <GenericBtn
+                  title="Done"
+                  sx={{
+                    height: '2rem',
+                    width: '5rem',
+                    fontSize: '1rem',
+                    background: `${THEME.colors.button.primary}`,
+                  }}
+                />
+              </form>
+            </UserFormProvider>
           }
         </ModalComp>
         <GenericBtn
