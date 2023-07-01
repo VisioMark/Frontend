430d184ab1d28c87d64ba75d26d916ae8dc1e5c6
Domeh John Kelvin <75913666+JhohannesK@users.noreply.github.com>
2023-07-01 03:04:51 +0000

    Feature/master key (#27)
    * able to take inputs into objcts
    
    * able to select choice of answer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,4 +7,52 @@ export const Constants = {
     settings: '/settings',
     allfiles: '/allfiles',
   },
+  ACADEMIC_LEVELS: [
+    {
+      value: '100',
+      label: '100',
+    },
+    {
+      value: '200',
+      label: '200',
+    },
+    {
+      value: '300',
+      label: '300',
+    },
+    {
+      value: '400',
+      label: '400',
+    },
+    {
+      value: '500',
+      label: '500',
+    },
+    {
+      value: '600',
+      label: '600',
+    },
+    {
+      value: '700',
+      label: '700',
+    },
+  ],
+  ACADEMIC_YEAR: [
+    {
+      value: '2020/2021',
+      label: '2020/2021',
+    },
+    {
+      value: '2021/2022',
+      label: '2021/2022',
+    },
+    {
+      value: '2022/2023',
+      label: '2022/2023',
+    },
+    {
+      value: '2023/2024',
+      label: '2023/2024',
+    },
+  ],
 };
